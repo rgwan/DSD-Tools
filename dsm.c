77a07667b528c22506ff896e7e3d94222c31ef90
Zhiyuan Wan <rgwan@rocaloid.org>
2015-12-13 01:21:24 +0800

    add input clipper

--- dsm.c
+++ dsm.c
@@ -18,10 +18,14 @@ int acc_2 = 0;//Integrator 2
 char DSD_ConvertSamples_2(short int input)
 {//2 order Delta_Sigma Modulator
 	int feedback;//which is 1-bit DAC
+	if(input > 32767)
+		input = 32767;
+	if(input < -32767)
+		input = -32767;
 	if(acc_2 > 0)
-		feedback = 32768;
+		feedback = 32767;
 	else
-		feedback = -32768;
+		feedback = -32767;
 	acc_1 += input - feedback;
 	acc_2 += acc_1 - feedback;
 	if(acc_2 > 0)
